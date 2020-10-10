ab72b1bfec3b31c3011a7fa357aaf1ad1552ac02
mqchecker724 <mqchecker724@gmail.com>
2020-10-10 19:53:43 +0400

    Updated Settings

--- mqchecker/settings.py
+++ mqchecker/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm9k0hi4p6*^)m8r5xbf*&^hkt877ay^dgm@vdx*tk_&^xt&!!f'
+#SECRET_KEY = 'm9k0hi4p6*^)m8r5xbf*&^hkt877ay^dgm@vdx*tk_&^xt&!!f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mqchecker.herokuapp.com']
 
 
 # Application definition
