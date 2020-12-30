cfdcc8c3ae3a6a17f99b92007fff8a013bfe5745
mqchecker724 <mqchecker724@gmail.com>
2020-12-30 15:20:12 +0400

    commit message

--- mqchecker/settings.py
+++ mqchecker/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+#DEBUG = 'True'
 
-ALLOWED_HOSTS = ['mqchecker.herokuapp.com']
+ALLOWED_HOSTS = ['mqchecker.herokuapp.com','localhost']
 
 
 # Application definition
